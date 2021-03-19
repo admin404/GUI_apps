9e75bc625d659101ff5d6579290b3444560d514f
admin404 <neterror404@gmail.com>
2021-03-19 15:31:39 +0200

    15:31

--- project_starter/main.py
+++ project_starter/main.py
@@ -5,25 +5,28 @@ from PySide6.QtGui import *
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 
+
 class main(QMainWindow):
-	def __init__(self):
-		super(main, self).__init__()
-		self.setCentralWidget(widget())
+    def __init__(self):
+        super(main, self).__init__()
+        self.setCentralWidget(widget())
+
 
 class widget(QWidget):
-	def __init__(self):
-		super().__init__()
-		self.ui()
+    def __init__(self):
+        super().__init__()
+        self.ui()
 
-	def ui(self):
-		self.btn=QPushButton()
-		self.btn.setText("Hello World")
+    def ui(self):
+        self.btn = QPushButton()
+        self.btn.setText("Hello World")
 
-		self.layout=QGridLayout(self)
-		self.layout.addWidget(self.btn)
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(self.btn)
+
 
 if __name__ == "__main__":
-	app = QApplication(sys.argv)
-	win = main()
-	win.show()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    win = main()
+    win.show()
+    sys.exit(app.exec_())
