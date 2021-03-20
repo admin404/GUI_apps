32d72ab3e7734ab5c7cfcedacbec595d0d627a8d
admin404 <neterror404@gmail.com>
2021-03-20 20:20:52 +0200

    20:20

--- project_starter/main.py
+++ project_starter/main.py
@@ -2,9 +2,9 @@ import pyautogui
 import sys
 import os
 
-from PySide6.QtGui import *
-from PySide6.QtCore import *
-from PySide6.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
 
 
 # mouse position function
---
+++ project_starter/update_test.py
@@ -0,0 +1,6 @@
+import time
+
+# import time
+# for i in range(10):
+#     print('Update %d' % i, end='\r')
+#     time.sleep(5)
