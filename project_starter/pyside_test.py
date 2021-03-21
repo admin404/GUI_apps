5c6f8498bf4c7e2155d046deb1dd42dd64955eda
admin404 <neterror404@gmail.com>
2021-03-21 09:36:14 +0200

    09:36

---
+++ project_starter/pyside_test.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+
+class main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setCentralWidget(widget())
+
+
+class widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui()
+
+    def ui(self):
+        self.btn = QPushButton()
+        self.btn.setText("Hello")
+        self.btn.clicked.connect(self.say_hello)
+
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(self.btn)
+
+    def say_hello(self):
+        print("Hello")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = main()
+    window.show()
+    sys.exit(app.exec_())
--- project_starter/update_test.py
+++
@@ -1,6 +0,0 @@
-import time
-
-# import time
-# for i in range(10):
-#     print('Update %d' % i, end='\r')
-#     time.sleep(5)
