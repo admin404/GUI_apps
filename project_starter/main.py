878cb1bfe7ad32f34b4669762b8bcf7c09012cd9
admin404 <neterror404@gmail.com>
2021-03-22 14:11:51 +0200

    14:!1

--- project_starter/main.py
+++ project_starter/main.py
@@ -2,9 +2,59 @@ import pyautogui
 import sys
 import os
 
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
-from PyQt6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+
+
+class thread(QThread):
+	trig = Signal()
+	def __init__(self):
+		super().__init__()
+		self.flag = 1
+
+
+	def run(self):
+		print("Starting")
+		mouse_position()
+
+
+	def stop(self):
+		print("Stopping")
+
+
+class main(QMainWindow):
+    def __init__(self):
+        super(main, self).__init__()
+        self.setWindowTitle("Project Starter")
+        self.setCentralWidget(widget())
+
+
+class widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui()
+
+    def ui(self):
+        self.btn = QPushButton()
+        self.btn.setText("Start")
+        self.btn.pressed.connect(self.action_handler)
+
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(self.btn)
+
+    def action_handler(self):
+        sg = self.btn.text()
+        if sg == 'Start':
+        	self.thread = thread()
+        	self.thread.start()
+        	self.btn.setText('Stop')
+
+        else:
+        	self.btn.setText('Start')
+        	self.thread.stop()
+
+
 
 
 # mouse position function
@@ -29,32 +79,8 @@ class mouse_position():
             _ = os.system('clear')
 
 
-class main(QMainWindow):
-    def __init__(self):
-        super(main, self).__init__()
-        self.setWindowTitle("Project Starter")
-        self.setCentralWidget(widget())
-
-
-class widget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.ui()
-
-    def ui(self):
-        self.submit = QPushButton()
-        self.submit.setText("Submit")
-        self.submit.pressed.connect(self.action_handler)
-
-        self.layout = QGridLayout(self)
-        self.layout.addWidget(self.submit)
-
-    def action_handler(self):
-        mouse_position()
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = main()
     win.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
